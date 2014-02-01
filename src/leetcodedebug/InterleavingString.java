0210c6ea75ea7fb87d655328bc630a64ae84a784
Shi Chen <cscs54cs@gmail.com>
2014-01-31 19:37:00 -0500

    some new DP questions solution Signed-off-by: Shi Chen <cscs54cs@gmail.com>

---
+++ src/leetcodedebug/InterleavingString.java
@@ -0,0 +1,38 @@
+package leetcodedebug;
+
+
+
+public class InterleavingString {
+	public boolean isInterleave(String s1, String s2, String s3) {
+        if((s1.length()+s2.length())!=s3.length())    return false;
+        boolean[][] dp = new boolean[s1.length()+1][s2.length()+1];
+        dp[0][0]=true;
+        for(int i=1;i<=s1.length();i++)
+        {
+        	
+            dp[i][0]=(s1.charAt(i-1)==s3.charAt(i-1))&&dp[i-1][0];
+        }
+        for(int i=1;i<=s2.length();i++)
+        {
+        	System.out.println(s3.charAt(i-1)+"  "+s2.charAt(i-1));
+            dp[0][i]=(s2.charAt(i-1)==s3.charAt(i-1))&&dp[0][i-1];
+        }
+        dp[0][0]=true;
+        for(int i=1;i<=s1.length();i++)
+        {
+            for(int j=1;j<=s2.length();j++)
+            {
+                dp[i][j]=(dp[i-1][j]&&(s1.charAt(i-1)==s3.charAt(i+j-1)))||(dp[i][j-1]&&(s2.charAt(j-1)==s3.charAt(i+j-1)));
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+	public static void main(String[] args)
+	{
+		InterleavingString test = new InterleavingString();
+		String a = "";
+		String b = "a";
+		String c = "a";
+		test.isInterleave(a, b, c);
+	}
+}
